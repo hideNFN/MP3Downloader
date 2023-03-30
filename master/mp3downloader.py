b7fbd589810b59b835006cfaf207c66881f1714f
Raul <80535950+hideNFN@users.noreply.github.com>
2023-03-30 19:37:10 +0300

    Optimized code

--- master/mp3downloader.py
+++ master/mp3downloader.py
@@ -7,23 +7,24 @@ ffmpegfolder = datafolder + "\\.ffmpeg"
 ffmpegcheck = ffmpegfolder + "\\ffmpeg.exe"
 ffprobecheck = ffmpegfolder + "\\ffprobe.exe"
 
+
+if os.path.exists(datafolder) is False or os.path.exists(ffmpegfolder) is False:
+    print("\nCreating necessary folders...")
+    if os.path.exists(datafolder) is False:
+        os.mkdir(datafolder)
+    if os.path.exists(ffmpegfolder) is False:
+        os.mkdir(ffmpegfolder)
+
+if os.path.exists(ffmpegcheck) is True and os.path.exists(ffprobecheck) is True:
+    print("\nThe FFmpeg install has been found.\n")
+else:
+    print('\nThe FFmpeg install hasn\'t been found, please download FFmpeg and place ffmpeg.exe and ffprobe.exe inside the "\.data\.ffmpeg" path.\n')
+
+os.chdir(datafolder)
+
 def mp3dwn():
-    print("MP3 Downloader v1.1.0 by hideNFN\n")
+    print("\nMP3 Downloader v1.1.0 by hideNFN\n")
 
-    if os.path.exists(datafolder) is False or os.path.exists(ffmpegfolder) is False:
-        print("\nCreating necessary folders...")
-        if os.path.exists(datafolder) is False:
-            os.mkdir(datafolder)
-        if os.path.exists(ffmpegfolder) is False:
-            os.mkdir(ffmpegfolder)
-
-    if os.path.exists(ffmpegcheck) is True and os.path.exists(ffprobecheck) is True:
-        print("\nThe FFmpeg install has been found.")
-    else:
-        print('\nThe FFmpeg install hasn\'t been found, please download FFmpeg and place ffmpeg.exe and ffprobe.exe inside the "\.data\.ffmpeg" path.')
-
-    os.chdir(datafolder)
-    
     while True:
         print("\nPaste the URL of the media you would like to download in a mp3 format:\n")
 
